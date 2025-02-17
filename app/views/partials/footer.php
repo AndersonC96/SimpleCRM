d00a17f92291587ebde4008e0ecd34e32ecdebeb
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-02-17 14:28:10 -0300

    Create footer.php

---
+++ app/views/partials/footer.php
@@ -0,0 +1,7 @@
+        <footer class="bg-dark text-white mt-5 p-4 text-center">
+            <p>&copy; <?= date('Y') ?> Simple Pharma. Todos os direitos reservados.</p>
+        </footer>
+        <!-- Bootstrap JS Bundle -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
