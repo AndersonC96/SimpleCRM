73d251c2ec400958020f2117f6a897cc4e5d4e40
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-02-17 14:26:43 -0300

    Create navbar.php

---
+++ app/views/partials/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Meu Projeto</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <!-- Itens do menu -->
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?url=auth/login">Login</a>
+                </li>
+                <!-- Outras páginas -->
+            </ul>
+        </div>
+    </div>
+</nav>
