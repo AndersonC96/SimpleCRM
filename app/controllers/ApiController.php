63f71dfbcab39440b6c01637944ddc514ffc5136
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-02-18 18:09:05 -0300

    Delete

--- app/controllers/ApiController.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class ApiController {
-        public function getCampaigns() {
-            header('Content-Type: application/json');
-            require_once 'app/models/Campaign.php';
-            $campaigns = Campaign::getAll();
-            echo json_encode($campaigns);
-        }
-    }
