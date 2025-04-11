ac30545e2001d69d77e5e5ee09fb446a71d9e754
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 15:51:50 -0300

    Update SurveyController.php

--- app/Controllers/SurveyController.php
+++ app/Controllers/SurveyController.php
@@ -2,38 +2,31 @@
 	namespace Tisim\SimpleCrm\Controllers;
 	use Tisim\SimpleCrm\Models\Survey;
 	class SurveyController extends BaseController {
-		public function index() {
-			$surveys = Survey::all();
-			$this->view('surveys/index', compact('surveys'));
-		}
-		public function create() {
-			$this->view('surveys/create');
-		}
-		public function store() {
-			$survey = new Survey;
-			$survey->title = $_POST['title'];
-			$survey->description = $_POST['description'];
-			$survey->save();
-			$this->redirect('surveys');
-		}
-		public function show($id) {
-			$survey = Survey::find($id);
-			$this->view('surveys/show', compact('survey'));
-		}
-		public function delete($id) {
-			$survey = Survey::find($id);
-			$survey?->delete();
-			$this->redirect('surveys');
+    	public function index() {
+        	$surveys = Survey::all();
+        	$this->view('surveys/index', compact('surveys'));
     	}
-		public function store() {
-    		$survey = new Survey;
-    		$survey->title = $_POST['title'];
-    		$survey->description = $_POST['description'];
-    		$survey->save();
-		    // Gera popup com redirecionamento usando JS
-    		echo "<script>
-        		alert('Pesquisa criada com sucesso!');
-        		window.location.href = 'index.php?url=surveys';
-    		</script>";
-		}
+	    public function create() {
+        	$this->view('surveys/create');
+    	}
+	    public function store() {
+        	$survey = new Survey;
+        	$survey->title = $_POST['title'];
+        	$survey->description = $_POST['description'];
+        	$survey->save();
+	        // Gera popup com redirecionamento usando JS
+    	    echo "<script>
+        	    alert('Pesquisa criada com sucesso!');
+            	window.location.href = 'index.php?url=surveys';
+        	</script>";
+    	}
+	    public function show($id) {
+        	$survey = Survey::find($id);
+        	$this->view('surveys/show', compact('survey'));
+    	}
+	    public function delete($id) {
+        	$survey = Survey::find($id);
+        	$survey?->delete();
+        	$this->redirect('index.php?url=surveys');
+    	}
 	}
