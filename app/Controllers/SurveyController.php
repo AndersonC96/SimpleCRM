9d9b63875c4e042129b8fba140300a3c7b70e657
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 12:02:20 -0300

    Create SurveyController.php

---
+++ app/Controllers/SurveyController.php
@@ -0,0 +1,28 @@
+<?php
+	namespace Tisim\SimpleCrm\Controllers;
+	use Tisim\SimpleCrm\Models\Survey;
+	class SurveyController extends BaseController {
+		public function index() {
+			$surveys = Survey::all();
+			$this->view('surveys/index', compact('surveys'));
+		}
+		public function create() {
+			$this->view('surveys/create');
+		}
+		public function store() {
+			$survey = new Survey;
+			$survey->title = $_POST['title'];
+			$survey->description = $_POST['description'];
+			$survey->save();
+			$this->redirect('surveys');
+		}
+		public function show($id) {
+			$survey = Survey::find($id);
+			$this->view('surveys/show', compact('survey'));
+		}
+		public function delete($id) {
+			$survey = Survey::find($id);
+			$survey?->delete();
+			$this->redirect('surveys');
+    		}
+	}
