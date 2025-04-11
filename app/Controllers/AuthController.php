55acecd75627e348f263e4bc9958c6ec749967f6
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 14:16:59 -0300

    Create AuthController.php

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+    namespace Tisim\SimpleCrm\Controllers;
+    use Tisim\SimpleCrm\Models\User;
+    class AuthController extends BaseController {
+        public function showLogin() {
+            $this->view('auth/login');
+        }
+        public function login() {
+            $user = User::where('email', $_POST['email'])->first();
+            if ($user && password_verify($_POST['password'], $user->password)) {
+                $_SESSION['user'] = $user->id;
+                $this->redirect('surveys');
+            } else {
+                $this->view('auth/login', ['error' => 'E-mail ou senha inválidos.']);
+            }
+        }
+        public function showRegister() {
+            $this->view('auth/register');
+        }
+        public function register() {
+            $user = new User;
+            $user->name = $_POST['name'];
+            $user->email = $_POST['email'];
+            $user->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $user->save();
+            $_SESSION['user'] = $user->id;
+            $this->redirect('surveys');
+        }
+        public function logout() {
+            session_destroy();
+            $this->redirect('/');
+        }
+    }
