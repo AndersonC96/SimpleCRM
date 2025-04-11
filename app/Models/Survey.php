4595bdb459ed07e2d964f7ed79303c42ee755b14
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 16:43:53 -0300

    Update Survey.php

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -1,10 +1,13 @@
 <?php
     namespace Tisim\SimpleCrm\Models;
+    use Tisim\SimpleCrm\Models\Question;
     class Survey extends BaseModel {
         protected $table = 'surveys';
         protected $fillable = [
             'title',
             'description'
         ];
+        public function questions() {
+            return $this->hasMany(Question::class);
+        }
     }
-?>
