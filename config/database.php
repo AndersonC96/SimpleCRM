ac3acd176b29e4cd50389517b2f6d211b734b976
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 12:58:40 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,14 @@
 <?php
     use Illuminate\Database\Capsule\Manager as Capsule;
+    // Carrega variáveis do .env
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
+    // Configura conexão
     $capsule = new Capsule;
     $capsule->addConnection([
         'driver'    => $_ENV['DB_CONNECTION'],
         'host'      => $_ENV['DB_HOST'],
+        'port'      => $_ENV['DB_PORT'],
         'database'  => $_ENV['DB_DATABASE'],
         'username'  => $_ENV['DB_USERNAME'],
         'password'  => $_ENV['DB_PASSWORD'],
