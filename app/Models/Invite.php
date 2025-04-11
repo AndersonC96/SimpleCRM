9e488863e51082cc53bba54b5f7cbfb4dba3d300
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 17:22:42 -0300

    Update Invite.php

--- app/Models/Invite.php
+++ app/Models/Invite.php
@@ -2,11 +2,21 @@
     namespace Tisim\SimpleCrm\Models;
     class Invite extends BaseModel {
         protected $table = 'invites';
-        protected $fillable = ['survey_id', 'respondent_id', 'token', 'sent_at', 'channel'];
+        protected $fillable = [
+            'survey_id',
+            'respondent_id',
+            'token',
+            'sent_at',
+            'channel',
+            'representative_id'
+        ];
         public function survey() {
             return $this->belongsTo(Survey::class);
         }
         public function responses() {
             return $this->hasMany(Response::class);
+        }
+        public function representative() {
+            return $this->belongsTo(Representative::class);
         }
     }
