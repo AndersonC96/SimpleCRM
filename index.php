fc45c806ee99377ed9b3c0d5b43008b6432a2ea5
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 11:50:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
     require __DIR__ . '/vendor/autoload.php';
-    use App\Controllers\HomeController;
-    // Roteamento básico
-    $uri = $_SERVER['REQUEST_URI'];
-    if ($uri === '/' || $uri === '/index.php') {
-        (new HomeController())->index();
-        exit;
-    }
-    // adicionar mais rotas aqui
-    http_response_code(404);
-    echo "Página não encontrada";
+    require __DIR__ . '/config/database.php';
+    require __DIR__ . '/routes/web.php';
+    use Tisim\SimpleCrm\Controllers\SurveyController;
+    // Simples roteador baseado em GET
+    $uri = $_GET['url'] ?? '/';
+    switch ($uri) {
+        case '/':
+            echo "<h1>Bem-vindo ao SimpleCRM!</h1>";
+            break;
+        case 'surveys':
+            $controller = new SurveyController();
+            $controller->index();
+        break;
+        default:
+            http_response_code(404);
+            echo "Página não encontrada.";
+        }
 ?>
