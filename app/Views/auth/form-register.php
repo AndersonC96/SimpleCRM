1c8794c746489091fc937634a3cacc4f114f2c35
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 14:21:23 -0300

    Create form-register.php

---
+++ app/Views/auth/form-register.php
@@ -0,0 +1,22 @@
+<form method="POST" action="register">
+    <div class="field">
+        <label class="label">Nome</label>
+        <div class="control">
+            <input class="input" type="text" name="name" required>
+        </div>
+    </div>
+    <div class="field">
+        <label class="label">E-mail</label>
+        <div class="control">
+            <input class="input" type="email" name="email" required>
+        </div>
+    </div>
+    <div class="field">
+        <label class="label">Senha</label>
+        <div class="control">
+            <input class="input" type="password" name="password" required>
+        </div>
+    </div>
+    <button class="button is-success">Cadastrar</button>
+</form>
+<p class="mt-3">Já tem conta? <a href="login">Fazer login</a></p>
