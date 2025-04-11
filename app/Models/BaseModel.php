9754e88607b56294d08e9470274c4f8b76b89c3e
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 12:05:11 -0300

    Create BaseModel.php

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,7 @@
+<?php
+    namespace Tisim\SimpleCrm\Models;
+    use Illuminate\Database\Eloquent\Model;
+    class BaseModel extends Model {
+        // Comum a todos os modelos (se quiser)
+    }
+?>
