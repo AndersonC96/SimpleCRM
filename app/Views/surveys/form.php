caea2358b6921f3b1218b93e354450c3fab7e9f2
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 15:08:44 -0300

    Update form.php

--- app/Views/surveys/form.php
+++ app/Views/surveys/form.php
@@ -1,18 +1,16 @@
-<form method="POST" action="surveys/store">
+<form method="POST" action="index.php?url=surveys/store">
     <div class="field">
         <label class="label">Título</label>
         <div class="control">
             <input class="input" type="text" name="title" required>
         </div>
     </div>
-
     <div class="field">
         <label class="label">Descrição</label>
         <div class="control">
             <textarea class="textarea" name="description" required></textarea>
         </div>
     </div>
-
     <div class="field">
         <div class="control">
             <button class="button is-success">Salvar</button>
