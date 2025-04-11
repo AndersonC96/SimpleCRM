277809a3a5e74578d28d571ded542563370aee6f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 12:39:41 -0300

    Create view.php

---
+++ app/Views/surveys/view.php
@@ -0,0 +1,3 @@
+<p><strong>Título:</strong> <?= htmlspecialchars($survey->title) ?></p>
+<p><strong>Descrição:</strong> <?= htmlspecialchars($survey->description) ?></p>
+<p><strong>Criada em:</strong> <?= $survey->created_at ?></p>
