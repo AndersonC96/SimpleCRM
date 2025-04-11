d2b68bb298077b86259050794472b98e978154e5
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-11 15:19:13 -0300

    Update list.php

--- app/Views/surveys/list.php
+++ app/Views/surveys/list.php
@@ -1,10 +1,11 @@
-<a href="index.php?url=surveys/create" class="button is-primary mb-3">Nova Pesquisa</a>
+<a href="index.php?url=surveys/create" class="button is-primary mb-4">Nova Pesquisa</a>
 <table class="table is-fullwidth is-striped">
     <thead>
         <tr>
             <th>Título</th>
             <th>Descrição</th>
             <th>Criada em</th>
+            <th>Ações</th>
         </tr>
     </thead>
     <tbody>
@@ -13,6 +14,10 @@
                 <td><?= htmlspecialchars($survey->title) ?></td>
                 <td><?= htmlspecialchars($survey->description) ?></td>
                 <td><?= $survey->created_at ?></td>
+                <td>
+                    <a class="button is-small is-info" href="index.php?url=surveys/show&id=<?= $survey->id ?>">Ver</a>
+                    <a class="button is-small is-danger" href="index.php?url=surveys/delete&id=<?= $survey->id ?>" onclick="return confirm('Tem certeza que deseja excluir?')">Excluir</a>
+                </td>
             </tr>
         <?php endforeach; ?>
     </tbody>
