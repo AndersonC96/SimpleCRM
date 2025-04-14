bf29fd289d1c8c6ac8c9bbb3096e71764a2ce1ad
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-14 11:03:50 -0300

    Create UsuarioController.php

---
+++ controller/UsuarioController.php
@@ -0,0 +1,23 @@
+<?php
+    namespace App\Controller;
+    use App\Core\View;
+    class UsuarioController {
+        public function index() {
+            // Listar usuários
+        }
+        public function create() {
+            // Formulário de novo usuário
+        }
+        public function store() {
+            // Processa novo usuário
+        }
+        public function edit($id) {
+            // Formulário de edição
+        }
+        public function update($id) {
+            // Atualiza dados do usuário
+        }
+        public function delete($id) {
+            // Deleta usuário
+        }
+    }
