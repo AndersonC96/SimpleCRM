e734b89aea856974d627e852bfa647a6422c8ac2
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-14 18:30:09 -0300

    Update DashboardController.php

--- controller/DashboardController.php
+++ controller/DashboardController.php
@@ -1,12 +1,16 @@
 <?php
     namespace App\Controller;
     use App\Core\View;
+    use App\Repository\RepresentanteRepository;
     class DashboardController {
         public function index() {
             if (!isset($_SESSION['usuario'])) {
                 header("Location: index.php?url=login");
                 exit;
             }
-            View::render('dashboard/index', ['usuario' => $_SESSION['usuario']]);
+            /*View::render('dashboard/index', ['usuario' => $_SESSION['usuario']]);*/
+            $representantes = RepresentanteRepository::listarComDadosNPS();
+            $areas = RepresentanteRepository::listarAreas();
+            View::render('dashboard/index', compact('representantes', 'areas'));
         }
     }
