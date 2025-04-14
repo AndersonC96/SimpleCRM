3a3c2dc3a0a1a67f298564223dcfc389e76068a9
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-14 10:55:07 -0300

    Create app.php

---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+    return [
+        'app_name' => 'Sistema NPS WhatsApp',
+        'timezone' => 'America/Sao_Paulo',
+        'debug' => true,
+        'log_path' => __DIR__ . '/../logs/access.log',
+    ];
