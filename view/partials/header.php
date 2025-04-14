b6998dcc7cb6bc736183dcfc816d2b3cf1b926c7
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-14 11:47:15 -0300

    Create header.php

---
+++ view/partials/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Sistema NPS</title>
+  <link href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+  <nav class="navbar">
+    <a class="navbar-item" href="index.php?url=dashboard">Dashboard</a>
+    <a class="navbar-item" href="index.php?url=usuarios">Usuários</a>
+    <a class="navbar-item" href="index.php?url=clientes">Clientes</a>
+    <a class="navbar-item" href="index.php?url=campanhas">Campanhas</a>
+    <a class="navbar-item" href="index.php?url=logout">Sair</a>
+  </nav>
+  <hr>
