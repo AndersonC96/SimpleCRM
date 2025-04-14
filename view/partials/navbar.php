a1fe7bf90d59d844c332ffd2a606cb5bc3764668
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-14 11:48:32 -0300

    Create navbar.php

---
+++ view/partials/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar is-light mb-4" role="navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="index.php?url=dashboard">
+      📊 NPS System
+    </a>
+  </div>
+
+  <div class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="index.php?url=dashboard">Dashboard</a>
+      <a class="navbar-item" href="index.php?url=usuarios">Usuários</a>
+      <a class="navbar-item" href="index.php?url=clientes">Clientes</a>
+      <a class="navbar-item" href="index.php?url=campanhas">Campanhas</a>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <a class="button is-danger is-light" href="index.php?url=logout">Sair</a>
+      </div>
+    </div>
+  </div>
+</nav>
