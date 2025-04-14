bc9c1801e455e6cdf75380605958f9ebc0fac38b
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-14 11:04:32 -0300

    Create ClienteController.php

---
+++ controller/ClienteController.php
@@ -0,0 +1,20 @@
+<?php
+    namespace App\Controller;
+    use App\Core\View;
+    class ClienteController {
+        public function index() {
+            // Lista de clientes
+        }
+        public function importar() {
+            // Importação via Excel
+        }
+        public function exportar() {
+            // Exportação
+        }
+        public function criar() {
+            // Tela de novo cliente
+        }
+        public function salvar() {
+            // Salvamento no banco
+        }
+    }
