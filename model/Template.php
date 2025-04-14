909dbdf5fbcb116ccc66d4001121042f6ee69393
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-14 11:33:56 -0300

    Create Template.php

---
+++ model/Template.php
@@ -0,0 +1,9 @@
+<?php
+    namespace App\Model;
+    class Template {
+        public int $id;
+        public string $nome;
+        public string $conteudo_html;
+        public string $versao;
+        public string $criado_em;
+    }
