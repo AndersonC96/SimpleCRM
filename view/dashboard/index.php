122457f49289895f449b0c4d47080a8feaca9eda
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-14 11:37:22 -0300

    Create index.php

---
+++ view/dashboard/index.php
@@ -0,0 +1,4 @@
+<?php include 'view/partials/header.php'; ?>
+<h2>Bem-vindo, <?= htmlspecialchars($usuario['nome']) ?>!</h2>
+<p>Esse é o seu painel inicial.</p>
+<?php include 'view/partials/footer.php'; ?>
