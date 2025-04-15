c9c36d9b8a5640594e46139ee2b0618f4532f1ec
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-15 18:05:53 -0300

    Update DashboardController.php

--- controller/DashboardController.php
+++ controller/DashboardController.php
@@ -2,6 +2,7 @@
     namespace App\Controller;
     use App\Core\View;
     use App\Repository\RepresentanteRepository;
+    use App\Repository\EngajamentoRepository;
     class DashboardController {
         public function index() {
             if (!isset($_SESSION['usuario'])) {
@@ -12,5 +13,12 @@
             $representantes = RepresentanteRepository::listarComDadosNPS();
             $areas = RepresentanteRepository::listarAreas();
             View::render('dashboard/index', compact('representantes', 'areas'));
+            $resumo = EngajamentoRepository::obterResumo();
+            $respostasSemana = EngajamentoRepository::respostasPorDia();
+            View::render('dashboard/index', [
+                'resumo' => $resumo,
+                'respostasSemana' => $respostasSemana,
+                // + outras variáveis
+            ]);
         }
     }
