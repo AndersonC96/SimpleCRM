96793f230b21871ebfe8415e59b6084117c7726a
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-16 16:38:34 -0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,15 @@
 <?php
     return [
         'GET' => [
-            'login' => 'AuthController@login',
-            'dashboard' => 'DashboardController@index',
+            'login'      => 'AuthController@login',
+            'dashboard'  => 'DashboardController@index',
+            // 👇 Novas rotas para o módulo de usuários
+            'usuarios'           => 'UsuarioController@index',
+            'usuarios/create'    => 'UsuarioController@create',
         ],
         'POST' => [
-            'login' => 'AuthController@auth',
+            'login'              => 'AuthController@auth',
+            // 👇 Para salvar novo usuário
+            'usuarios/store'     => 'UsuarioController@store',
         ]
     ];
