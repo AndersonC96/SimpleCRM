8c83e9a09ad21c7899a1e1cb6c24ff56c50c73de
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-04-16 15:08:56 -0300

    Update Database.php

--- core/Database.php
+++ core/Database.php
@@ -3,7 +3,7 @@
     use PDO;
     use PDOException;
     class Database {
-        public static function connect(): PDO {
+        /*public static function connect(): PDO {
                 $host = $_ENV['DB_HOST'] ?? 'localhost';
             $dbname = $_ENV['DB_NAME'] ?? 'simplecrm';
             $user = $_ENV['DB_USER'] ?? 'root';
@@ -15,5 +15,24 @@
             } catch (PDOException $e) {
                 die("Erro ao conectar ao banco: " . $e->getMessage());
             }
+        }*/
+        private static ?PDO $instance = null;
+        public static function getInstance(): PDO {
+            if (self::$instance === null) {
+                $host = $_ENV['DB_HOST'];
+                $dbname = $_ENV['DB_NAME'];
+                $user = $_ENV['DB_USER'];
+                $pass = $_ENV['DB_PASS'];
+                $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+                try {
+                    self::$instance = new PDO($dsn, $user, $pass, [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    ]);
+                } catch (PDOException $e) {
+                    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+                }
+            }
+            return self::$instance;
         }
     }
